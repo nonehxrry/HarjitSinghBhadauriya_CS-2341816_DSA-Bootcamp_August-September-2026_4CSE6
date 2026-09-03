34e3aaabc9c9c2f4cf66a5378614ce75a745cd81
Harjit Singh Bhadauriya <163171644+nonehxrry@users.noreply.github.com>
2026-09-03 10:33:01 +0530

    Create 1116. Print Zero Even Odd.java

---
+++ Day 18/1116. Print Zero Even Odd.java
@@ -0,0 +1,40 @@
+class ZeroEvenOdd {
+    private int n;
+
+    private Semaphore zeroSemaphore = new Semaphore(1);
+    private Semaphore evenSemaphore = new Semaphore(0);
+    private Semaphore oddSemaphore = new Semaphore(0);
+
+    public ZeroEvenOdd(int n) {
+        this.n = n;
+    }
+
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 0; i < n; i++) {
+            zeroSemaphore.acquire();
+            printNumber.accept(0);
+
+            if (i % 2 == 0) {
+                oddSemaphore.release();
+            } else {
+                evenSemaphore.release();
+            }
+        }
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 2; i <= n; i += 2) {
+            evenSemaphore.acquire();
+            printNumber.accept(i);
+            zeroSemaphore.release();
+        }
+    }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i += 2) {
+            oddSemaphore.acquire();
+            printNumber.accept(i);
+            zeroSemaphore.release();
+        }
+    }
+}
